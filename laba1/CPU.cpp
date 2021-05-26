807d34b160fab8f0ed6f24d0db9c5797e9085969
Zayyya <pusia.zaya@gmail.com>
2021-05-26 22:14:44 +0300

    лабораторная работа по процессорам

---
+++ laba1/CPU.cpp
@@ -0,0 +1,11 @@
+#include "CPU.h"
+#include <cstdlib> 
+
+bool CPU::isFree()
+{
+	return (rand() % 100) < powerCPU;//рандомим от 0 до 100 и проверяет вырожение 
+}
+CPU::CPU(const int _power) : powerCPU(_power)
+{
+
+}
